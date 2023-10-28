e93a28bb12d00685eb0d47fa56052a68f344a2b6
Parikshit Akula <130651102+akulaparikshit@users.noreply.github.com>
2023-10-28 19:26:15 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Seaborn
+# 5. Tensorflow
+
+# ### Data Science Tools
+# | Tool             | Description                              |
+# |------------------|------------------------------------------|
+# | Jupyter Notebook | Interactive development environment      |
+# | VS Code          | Code editor with data science extensions |
+# | Git              | Version control for projects             |
+# | Tableau          | Data visualization tool                  | 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# a = 20
+# b = 10
+# 
+# add = a + b
+# subtraction = a - b
+# multiplication = a * b
+# division= a / b
+# 
+# add = 30
+# subtraction = 10
+# multiplication = 200
+# division= 2
+# 
+
+# In[3]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+exp = (3*4)+5
+print(exp)
+
+
+# In[4]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+print(200 / 6)
+
+
+# ### Objectives: List popular languages for Data Science.
+# 
+# * Python
+# * SQL
+# * R
+
+# # Author
+# 
+# Akula Parikshit
+
+# In[ ]:
+
+
+
+
